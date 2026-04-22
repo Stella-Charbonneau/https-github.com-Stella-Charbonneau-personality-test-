b03b192c3de369c9f430c54f4fecf79bbb7a5782
Stella Charbonneau <charbonneaustella@gmail.com>
2026-04-22 22:23:22 +0000

    Wed, Apr 22, 2026, 6:23 PM -04:00

--- personality_project.py
+++ personality_project.py
@@ -130,7 +130,7 @@ quiz = [
 
 def main():
     "A" == "anxious, competative, ambitous, foward looking"
-    "B" == "people orientated, organized, "
+    "B" == "people orientated, organized, emotially "
     "C" == "patient, relaxed, understanding"
     "D" == "ambicable, confident, energetic"
 
