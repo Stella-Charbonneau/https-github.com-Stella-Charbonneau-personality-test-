fbaa17bcdb053ecb8cef5a82d4125ccb062f481d
Stella Charbonneau <charbonneaustella@gmail.com>
2026-04-22 22:08:20 +0000

    Wed, Apr 22, 2026, 6:08 PM -04:00

--- personality_project.py
+++ personality_project.py
@@ -1,8 +1,30 @@
 # could do % wise personalities
 # could do visualizations
-scores:{
-    A:0
-    B:0
-    C:0
-    D:0
+
+scores = {
+    "A":0,
+    "B":0,
+    "C":0,
+    "D":0
+}
+
+quiz = [
+    {
+        "question": "What's your ideal weekend?",
+        "answers": {
+            "A": "Reading a book",
+            "B": "Going to a party",
+            "C": "Exploring nature",
+            "D": "Playing video games"
         }
+    },
+    {
+        "question": "Pick a color:",
+        "answers": {
+            "A": "Blue",
+            "B": "Red",
+            "C": "Green",
+            "D": "Purple"
+        }
+    }
+]
