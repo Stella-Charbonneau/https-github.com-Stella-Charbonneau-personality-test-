55a07be356d9db6a97201dd023690e5fec0a7eef
Stella Charbonneau <charbonneaustella@gmail.com>
2026-05-02 16:02:45 +0000

    Sat, May 2, 2026, 12:02 PM -04:00

--- personality_project.py
+++ personality_project.py
@@ -151,7 +151,7 @@ scores = {
     }
 
 
-for i, item in enumerate(quiz, start=1): 
+for i, item in enumerate(quiz, start=1): #quiz is a list, in which each element is a question (similarly to a dictionary), i lets you 
     print(f"Question{i}: {item['question']}")
     for letter, text in item["answers"].items():
         print(f"{letter}. {text}")
